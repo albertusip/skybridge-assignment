f2a82355257fa2a2cafaaf5f2a64b317c1857666
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2020-10-19 12:12:22 +0700

    Deployment

---
+++ build/precache-manifest.077caf1a18b2f8cafe40b89128eac0d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a54a9ad7a735826ada3fa7fbece991d7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62b1474d26300e67f688",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "b7d80ff9d9cb24acdec9",
+    "url": "/static/css/main.432b1f8d.chunk.css"
+  },
+  {
+    "revision": "62b1474d26300e67f688",
+    "url": "/static/js/2.4e5734ec.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/static/js/2.4e5734ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d80ff9d9cb24acdec9",
+    "url": "/static/js/main.c68ac6d0.chunk.js"
+  },
+  {
+    "revision": "1ef1a3fe7906a8713e6c",
+    "url": "/static/js/runtime-main.df4e989f.js"
+  }
+]);
