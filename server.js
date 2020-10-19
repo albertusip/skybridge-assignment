4f091f1458721c34ced2bcfb4318c86675f35e1d
Albertus Istora Pahala <albertus.istora@ti.ukdw.ac.id>
2020-10-19 12:17:35 +0700

    Deploy 2

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.use(function(req, res, next) {
     }
     res.redirect('https://' + req.headers.host + req.url);
 });
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/build/'));
 app.get(/.*/, function(req, res) {
-    res.sendFile(__dirname + '/dist/index.html');
+    res.sendFile(__dirname + '/build/index.html');
 });
 
 app.listen(port);
